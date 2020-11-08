f9618f8994a7e7feeff274e4dab71aa18b0ce5ab
linwenjun <linwenjun@163.com>
2020-11-08 14:07:03 +0000

    first commit

---
+++ WebApiStarter/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiStarter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [Route("")]
+        public ActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
